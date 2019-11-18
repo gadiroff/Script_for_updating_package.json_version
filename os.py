4741ec88d008e73d0d40989364f4b6dc6b1168fb
Ubuntu <ubuntu@ip-172-31-85-83.ec2.internal>
2019-11-18 16:52:16 +0000

    Increase package.json file version

---
+++ os.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import os
+my_home_dir = os.getenv('HOME')
+print(my_home_dir)
---
+++ version.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+
+v = 3
+
+class CharList(list):
+
+    def __init__(self, s):
+        list.__init__(self, s)
+
+    @property
+    def list(self):
+        return list(self)
+
+    @property
+    def string(self):
+        return "".join(self)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, int) and len(value) > v:
+            cls = type(self).__name__
+            raise ValueError("attempt to assign sequence of size {} to {} item of size 1".format(len(value), cls))
+        super(CharList, self).__setitem__(key, value)
+
+    def __str__(self):
+        return self.string
+
+    def __repr__(self):
+        cls = type(self).__name__
+        return "{}(\'{}\')".format(cls, self.string)
+
+with open('package.json', 'r+') as f:
+    all_ = f.readlines()
+    line_old = all_[2]
+    print(line_old)
+    line_new = CharList(line_old)
+    release_count = int(line_new[18])
+    if line_new[19] == '"':
+        release_count += 1
+        line_new[18]  = str(release_count)
+    elif line_new[19] <= '8':
+        release_count = int(line_new[19])
+        release_count += 1
+        line_new[19]  = str(release_count)
+    elif line_new[19] == '9':
+        release_count = int(line_new[18])
+        release_count += 1
+        line_new[18]  = str(release_count)
+        line_new[19] = '0'
+    print(line_new)
+
+s = open("package.json").read()
+s = s.replace(str(line_old), str(line_new))
+f = open("package.json", 'w')
+f.write(s)
+f.close()
+
+cmd = 'git add . && git commit -m "Increase package.json file version" && git push origin mastert'
+os.system(cmd)
+
+
+
+
+
+
+
